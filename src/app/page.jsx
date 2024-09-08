9d3c5fb6fe4946d2b3c49a8b5bfc1a5e4e264ef3
doraeyi <ch855118@gmail.com>
2024-09-08 23:50:28 +0800

    修改/app/page.js

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,20 @@
-
 import Image from "next/image";
 import { cookies } from "next/headers"
 import { signJwtAccessToken, verifyJwt } from '@/lib/jwt'
-import * as React from "react"
+
 import { redirect } from "next/navigation"
-import { Calendar } from "@/components/ui/calendar"
+
 const HomePage = () => {
   // const accessToken = cookies().get("token")?.value || ""
   // console.log("gg",!verifyJwt(accessToken))
   // if (!verifyJwt(accessToken)) {
   //   redirect("/login")
   // }
-  const [date, setDate] = React.useState<Date | undefined>(new Date())
+
   return (
     <div className="">
       首頁
-    
-    <Calendar
-    mode="single"
-    selected={date}
-    onSelect={setDate}
-    className="rounded-md border"
-  /></div>
+    </div>
   );
 }
 
