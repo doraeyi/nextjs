d2fccbf2a44c55c86a869c92452892b7904ebabc
doraeyi <ch855118@gmail.com>
2025-02-06 15:42:25 +0800

    修改登入3

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -5,7 +5,7 @@ import { signJwtAccessToken } from '@/lib/jwt';
 import { OAuth2Client } from 'google-auth-library';
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-
+ 
 export const POST = async (req) => {
   const body = await req.json();
   
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-
+ 
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useGoogleLogin } from '@react-oauth/google';
